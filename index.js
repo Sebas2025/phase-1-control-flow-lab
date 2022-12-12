9c554f91fa627eaa5d636ba88390aaa1b1f4cd5b
SebastainBernal <sebasair2025@gmail.com>
2022-12-12 16:35:29 -0700

    commit tree c8850d8115a442047d8bc17b68f2b3b05f79cdb8

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <=400) {
+    result = "This one is on me!";
+  }
+  if (feet >400 && feet <=2000) {
+     result = "That will be twenty bucks."
+  }
+  if(feet >=2001) {
+     result = "I will gladly take your thirty bucks.";
+  }
+  if(feet >=2501) {
+    result ="No can do.";
+  }
+  return result
+  }
+  
+
+
+function ternaryCheckCity(city){
+ return city==='NYC' ? "Ok, sounds good." : "No go.";
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) { 
+    case "generous":
+      return "Thank you so much.";
+      case 'not as generous':
+        return 'Thank you.';
+        default: 
+        return 'Bye.';
+  }
+  
 }
